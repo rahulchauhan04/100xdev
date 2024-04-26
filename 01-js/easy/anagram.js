b17d43ef14ca1e6737eb6827537603c27545fea3
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-04-26 16:37:34 +0530

    anagram.js done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,19 @@
 
 function isAnagram(str1, str2) {
 
+  const normalize = (str) => str.toLowerCase();
+  const cleanSortedStr = (str) => str.replace(/[^a-z]/g,'').split('').sort().join('');
+
+  const sortedStr1 = normalize(str1);
+  const sortedStr2 = normalize(str2);
+
+  if (sortedStr1.length !== sortedStr2.length) 
+  {
+    return false;  
+  }
+
+  return cleanSortedStr(sortedStr1) === cleanSortedStr(sortedStr2);
+  
 }
 
 module.exports = isAnagram;
