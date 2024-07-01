533472358d0e3f1fb0e180e055163da08af634ae
rahulchauhan04 <rahulg0004@gmail.com>
2024-07-01 16:22:50 +0530

    mongo

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,23 @@
+const { User } = require('../db');
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const username = req.header('username');
+    const password = req.header('password');
+    User.findOne = {
+        username: username,
+        password: password
+    } 
+    .then(function(value) {
+        if(value){
+            next();
+        } else {
+            res.status(401).json({
+                message: "User doesn't esist"
+            })
+        }
+    })
 }
 
 module.exports = userMiddleware;
